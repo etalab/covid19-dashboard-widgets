b76b62f2be7509a25189d9f06aaeae5ca8ee3d93
Alexandre Bulté <alexandre@bulte.net>
2021-01-22 14:43:24 +0100

    Add map

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import Vue from 'vue'
 import store from './store'
 import HelloWorld from './components/HelloWorld'
 import GoodBye from './components/GoodBye'
+import Map from './components/Map'
 
 Vue.config.productionTip = false
 
@@ -10,8 +11,10 @@ import vueCustomElement from 'vue-custom-element'
 
 Vue.use(vueCustomElement)
 
+// do we need this since we're using a singleton in each component?
 HelloWorld.store = store
 GoodBye.store = store
 
 Vue.customElement('hello-world', HelloWorld)
 Vue.customElement('good-bye', GoodBye)
+Vue.customElement('da-map', Map)
