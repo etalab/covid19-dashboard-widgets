5497ad289ab2926b9d8f2e911b44a58a711a1031
Alexandre Bulté <alexandre@bulte.net>
2021-01-22 10:24:09 +0100

    working POC

---
+++ src/components/GoodBye.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    <h1>Goodbye stranger {{ msg }}</h1>
+    <p><code>{{ count }}</code></p>
+    <p><a href="#">LIEN GODDBY</a></p>
+  </div>
+</template>
+
+<script>
+import store from '@/store'
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  computed: {
+    count () {
+      return store.state.count
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color:red;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
-import App from './App.vue'
+
+import store from './store'
+import HelloWorld from './components/HelloWorld'
+import GoodBye from './components/GoodBye'
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+import vueCustomElement from 'vue-custom-element'
+
+Vue.use(vueCustomElement)
+
+HelloWorld.store = store
+GoodBye.store = store
+
+Vue.customElement('hello-world', HelloWorld)
+Vue.customElement('good-bye', GoodBye)
