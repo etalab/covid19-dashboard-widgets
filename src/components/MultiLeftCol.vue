9646450d4f1d7184ccd58948d729f1cce2876efc
nicolas <nicolas_boeuf@hotmail.fr>
2021-05-03 17:11:03 +0200

    add multilines chart

---
+++ src/components/MultiLeftCol.vue
@@ -0,0 +1,264 @@
+<template>
+
+  <div class="l_col fr-col-12 fr-col-md-4 fr-col-lg-3">
+
+        <div data-box="loc">
+          <p class="l_box_title fr-text--xs fr-mb-1w">Localisation</p>
+          <p class="l_box_label fr-text--sm fr-text--bold fr-my-0">
+            <svg width="16" height="16" viewBox="0 0 16 16"><path d="M12.243 2.424c2.343 2.343 2.343 6.142 0 8.485L8 15.152l-4.243-4.243c-2.343-2.343-2.343-6.142 0-8.485C6.101.081 9.9.081 12.243 2.424zM4.7 3.367c-1.822 1.822-1.822 4.777 0 6.6l3.3 3.3 3.3-3.3c1.822-1.823 1.822-4.778 0-6.6-1.823-1.822-4.777-1.822-6.6 0zM8 5.333c.736 0 1.333.597 1.333 1.334C9.333 7.403 8.736 8 8 8s-1.333-.597-1.333-1.333c0-.737.597-1.334 1.333-1.334z" transform="translate(-550 -5417) translate(527 5237) translate(0 124) translate(23 56)"/></svg>
+            <span class="fr-ml-1v">{{localisation}}</span>
+          </p>
+        </div>
+        <div class="sep fr-my-3w"></div>
+        <div data-box="number">
+
+          <p class="l_box_title fr-text--xs fr-mb-1w">Mise à jour : {{date}}</p>
+
+          <p class="fr-text--sm fr-text--bold fr-mt-0 fr-mb-1w">{{names[0]}}</p>
+
+          <div class="l_box_number_container">
+            <p class="fr-text--lg fr-text--bold fr-m-0">{{convertNumberToHuman(values[0])}}</p>
+            <p class="l_box_trend l_box_label fr-m-0 fr-text--xs fr-text--bold" v-bind:class="{'down':isDown,'green':isGreen,'red':isRed,'blue':isBlue}">
+              <svg class="trend_ico" width="24" height="24" viewBox="0 0 24 24">
+                <path v-if="!isBlue" d="M19.071 4.929c3.903 3.903 3.903 10.239 0 14.142-3.903 3.903-10.239 3.903-14.142 0-3.903-3.903-3.903-10.239 0-14.142 3.903-3.903 10.239-3.903 14.142 0zm-2.828 2.828H7.757l3.182 3.182-4.242 4.243 2.121 2.121 4.243-4.242 3.182 3.182V7.757z" transform="translate(-902 -5664) translate(902 5664)"/>
+                <path v-if="isBlue" d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2zm4 11H8v2h8v-2zm0-4H8v2h8V9z" transform="translate(-1366 -5645) translate(1366 5645)"/>
+              </svg>
+              <span class="fr-ml-1v">{{convertNumberToHuman(evolvalues[0])}} % en 7j</span>
+            </p>
+          </div>
+
+          <p class="fr-text--sm fr-text--bold fr-mt-0 fr-mb-1w">{{names[1]}}</p>
+
+          <div class="l_box_number_container">
+            <p class="fr-text--lg fr-text--bold fr-m-0">{{convertNumberToHuman(values[1])}}</p>
+            <p class="l_box_trend l_box_label fr-m-0 fr-text--xs fr-text--bold" v-bind:class="{'down':isDown2,'green':isGreen2,'red':isRed2,'blue':isBlue2}">
+              <svg class="trend_ico" width="24" height="24" viewBox="0 0 24 24">
+                <path v-if="!isBlue2" d="M19.071 4.929c3.903 3.903 3.903 10.239 0 14.142-3.903 3.903-10.239 3.903-14.142 0-3.903-3.903-3.903-10.239 0-14.142 3.903-3.903 10.239-3.903 14.142 0zm-2.828 2.828H7.757l3.182 3.182-4.242 4.243 2.121 2.121 4.243-4.242 3.182 3.182V7.757z" transform="translate(-902 -5664) translate(902 5664)"/>
+                <path v-if="isBlue2" d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2zm4 11H8v2h8v-2zm0-4H8v2h8V9z" transform="translate(-1366 -5645) translate(1366 5645)"/>
+              </svg>
+              <span class="fr-ml-1v">{{convertNumberToHuman(evolvalues[1])}} % en 7j</span>
+            </p>
+          </div>
+
+        </div>
+
+        <div class="sep fr-my-3w"></div>
+        <div class="fr-mb-3w fr-mb-md-0" data-box="leg">
+          <p class="l_box_title fr-text--xs fr-mb-1w">Légende</p>
+          <div class="l_box_legende_container">
+            <div class="legende_dot"></div>
+            <span class="fr-text--sm fr-text--bold fr-ml-1v">{{capitalize(units[0])}}</span>
+          </div>
+          <div class="l_box_legende_container">
+            <div class="legende_dot" data-serie="2"></div>
+            <span class="fr-text--sm fr-text--bold fr-ml-1v">{{capitalize(units[1])}}</span>
+          </div>
+        </div>
+
+
+
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'MultiLeftCol',
+
+  data(){
+    return {
+      isDown:false,
+      isGreen:false,
+      isRed:false,
+      isBlue:false,
+      isDown2:false,
+      isGreen2:false,
+      isRed2:false,
+      isBlue2:false
+    }
+  },
+  props: {
+    date: String,
+    localisation: String,
+    values: Array,
+    units: Array,
+    names: Array,
+    evolcodes: Array,
+    evolvalues : Array,
+  },
+  computed: {
+
+  },
+  methods: {
+
+    convertNumberToHuman(float){
+      return parseFloat(float).toLocaleString()
+    },
+
+    capitalize(string){
+      if(string){
+        return string.charAt(0).toUpperCase() + string.slice(1)
+      }
+      
+    },
+
+    testEvolStyle(){
+
+      if(this.evolcodes[0]=="green"){
+        this.isGreen = true
+        this.isRed = false
+        this.isBlue = false
+        this.evolvalues[0] > 0 ? this.isDown = false : this.isDown = true
+      }else if(this.evolcodes[0]=="red"){
+        this.isGreen = false
+        this.isRed = true
+        this.isBlue = false
+        this.evolvalue[0] > 0 ? this.isDown = false : this.isDown = true
+      }else{
+        this.isGreen = false
+        this.isRed = false
+        this.isBlue = true
+        this.isDown = false
+      }
+
+      if(this.evolcodes[1]=="green"){
+        this.isGreen2 = true
+        this.isRed2 = false
+        this.isBlue2 = false
+        this.evolvalues[1] > 0 ? this.isDown2 = false : this.isDown2 = true
+      }else if(this.evolcodes[1]=="red"){
+        this.isGreen2 = false
+        this.isRed2 = true
+        this.isBlue2 = false
+        this.evolvalues[1] > 0 ? this.isDown2 = false : this.isDown2 = true
+      }else{
+        this.isGreen2 = false
+        this.isRed2 = false
+        this.isBlue2 = true
+        this.isDown2 = false
+      }
+
+    }
+
+  },
+
+  watch:{
+    evolcodes:function(){
+      this.testEvolStyle()
+    },
+    evolvalues:function(){
+      this.testEvolStyle()
+    }
+  },
+
+  created(){
+
+    if(this.evolcodes[0]=="green"){
+        this.isGreen = true
+        this.isRed = false
+        this.isBlue = false
+        this.evolvalues[0] > 0 ? this.isDown = false : this.isDown = true
+      }else if(this.evolcodes[0]=="red"){
+        this.isGreen = false
+        this.isRed = true
+        this.isBlue = false
+        this.evolvalue[0] > 0 ? this.isDown = false : this.isDown = true
+      }else{
+        this.isGreen = false
+        this.isRed = false
+        this.isBlue = true
+        this.isDown = false
+      }
+
+      if(this.evolcodes[1]=="green"){
+        this.isGreen2 = true
+        this.isRed2 = false
+        this.isBlue2 = false
+        this.evolvalues[1] > 0 ? this.isDown2 = false : this.isDown2 = true
+      }else if(this.evolcodes[1]=="red"){
+        this.isGreen2 = false
+        this.isRed2 = true
+        this.isBlue2 = false
+        this.evolvalues[1] > 0 ? this.isDown2 = false : this.isDown2 = true
+      }else{
+        this.isGreen2 = false
+        this.isRed2 = false
+        this.isBlue2 = true
+        this.isDown2 = false
+      }
+  },
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+  /* overload fonts path, to delete when parent has access */
+  @import "../../css/overload-fonts.css";
+  @import "../../css/dsfr.min.css";
+
+  .l_col{
+
+    .sep {
+      border-bottom:1px solid #E5E5E5;
+    }
+
+    .l_box_title{
+      color: #6b6b6b;
+    }
+    .l_box_label{
+      display: flex;
+      align-items: center;
+    }
+    .l_box_number_container {
+      display: flex;
+      justify-content: space-between;
+      .l_box_trend {
+        &.down {
+          .trend_ico {
+            transform: rotate(90deg);
+          }
+        }
+        &.green {
+          color: #357941;
+          .trend_ico {
+            path {
+              fill: #357941;
+            }
+          }
+        }
+        &.red {
+          color: #d80600;
+          .trend_ico {
+            path {
+              fill: #d80600;
+            }
+          }
+        }
+        &.blue {
+          color: #0768d5;
+          .trend_ico {
+            path {
+              fill: #0768d5;
+            }
+          }
+        }
+      }
+    }
+    .legende_dot{
+      width: 16px;
+      height: 16px;
+      border-radius: 50%;
+      background-color: #000091;
+      display: inline-block;
+      transform:translate(0,3px);
+      &[data-serie="2"]{
+        background-color: #df001b;
+      }
+    }
+  }
+
+
+
+</style>
---
+++ src/components/MultiLineChart.vue
@@ -0,0 +1,295 @@
+<template>
+
+  <div class="widget_container fr-grid-row" :class="(loading)?'loading':''" :data-display="display" :id="widgetId">
+    <MultiLeftCol :data-display="display" :localisation="selectedGeoLabel" :date="currentDate" :values="currentValues" :units="units" :names="names" :evolcodes="evolcodes" :evolvalues="evolvalues"></MultiLeftCol>
+    <div class="r_col fr-col-12 fr-col-md-8 fr-col-lg-9">
+      <div class="chart fr-ml-0 fr-ml-md-3w">
+        <canvas :id="chartId"></canvas>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import store from '@/store'
+import Chart from 'chart.js'
+import MultiLeftCol from '@/components/MultiLeftCol' 
+export default {
+  name: 'MultiLineChart',
+  components: {
+    MultiLeftCol
+  },
+  data(){
+    return {
+      indicateur_data:undefined,
+      indicateur_data2:undefined,
+      labels:[],
+      dataset:[],
+      dataset2:[],
+      widgetId:"",
+      chartId:"",
+      display:"",
+      localisation:"",
+      currentValues:[],
+      currentDate:"",
+      names:[],
+      units:[],
+      evolcodes:[],
+      evolvalues:[],
+      chart:undefined,
+      loading:true
+    }
+  },
+  props: {
+    indicateur1: String,
+    indicateur2: String
+  },
+  computed: {
+    selectedGeoLevel () {
+      return store.state.user.selectedGeoLevel
+    },
+    selectedGeoCode () {
+      return store.state.user.selectedGeoCode
+    },
+    selectedGeoLabel () {
+      return store.state.user.selectedGeoLabel
+    },
+
+  },
+  methods: {
+
+    async getData () {
+      var url = "https://data.widgets.dashboard.covid19.data.gouv.fr/"+this.indicateur1+".json"
+      const dataRequest = await fetch(url)
+      const data = await dataRequest.json()
+      this.indicateur_data = data
+
+      var url2 = "https://data.widgets.dashboard.covid19.data.gouv.fr/"+this.indicateur2+".json"
+      const dataRequest2 = await fetch(url2)
+      const data2 = await dataRequest2.json()
+      this.indicateur_data2 = data2
+
+      this.loading = false
+      this.createChart()
+    },
+
+    updateData () {
+
+      var self = this
+      
+      var geolevel = this.selectedGeoLevel
+      var geocode = this.selectedGeoCode
+
+      var geoObject
+      var geoObject2
+
+      if(geolevel === "France"){
+        geoObject = this.indicateur_data["france"][0]
+        geoObject2 = this.indicateur_data2["france"][0]
+      }else{
+        geoObject = this.indicateur_data[geolevel].find(obj => {
+          return obj["code_level"] === geocode
+        })
+        geoObject2 = this.indicateur_data2[geolevel].find(obj => {
+          return obj["code_level"] === geocode
+        })
+
+      }
+
+      this.names.length = 0
+      this.units.length = 0
+      this.currentValues.length = 0
+      this.evolcodes.length = 0
+      this.evolvalues.length = 0      
+
+      this.names.push(this.indicateur_data["nom"],this.indicateur_data2["nom"])
+      this.units.push(this.indicateur_data["unite"],this.indicateur_data2["unite"])
+      this.currentValues.push(geoObject["last_value"],geoObject2["last_value"])
+      this.currentDate = this.convertDateToHuman(geoObject["last_date"])
+      this.evolcodes.push(geoObject["evol_color"],geoObject2["evol_color"])
+      this.evolvalues.push(geoObject["evol_percentage"],geoObject2["evol_percentage"])
+
+      this.labels.length = 0
+      this.dataset.length = 0
+      this.dataset2.length = 0
+
+      geoObject["values"].forEach(function(d){
+        
+        self.labels.push(self.convertDateToHuman(d["date"]))
+        self.dataset.push((d["value"]))
+
+        var corresponding_value = geoObject2["values"].find(obj => {
+          return obj["date"] === d["date"]
+        })
+
+        self.dataset2.push(corresponding_value["value"])
+
+      })
+
+    },
+
+    updateChart () {
+      
+      this.updateData()
+      this.chart.update()
+    
+    },
+
+    createChart () {
+      var self = this
+    
+      this.updateData()
+      
+      var xTickLimit
+      this.display=== 'big' ? xTickLimit = 5 : xTickLimit = 1
+      
+      var ctx = document.getElementById(self.chartId).getContext('2d')
+
+      var gradientFill
+
+      this.display=== 'big' ? gradientFill = ctx.createLinearGradient(0, 0, 0, 500) : gradientFill = ctx.createLinearGradient(0, 0, 0, 250)
+
+      gradientFill.addColorStop(0, "rgba(218, 218, 254, 0.6)")
+      gradientFill.addColorStop(0.6, "rgba(245, 245, 255, 0)")
+
+      var gradientFill2
+      
+      this.display=== 'big' ? gradientFill2 = ctx.createLinearGradient(0, 0, 0, 350) : gradientFill2 = ctx.createLinearGradient(0, 0, 0, 225)
+
+      gradientFill2.addColorStop(0, "rgba(223, 0, 27, 0.6)")
+      gradientFill2.addColorStop(0.6, "rgba(223, 0, 27, 0)")
+
+      this.chart = new Chart(ctx, {
+          data: {
+              labels: self.labels,
+              datasets: [
+                {
+                  data: self.dataset,
+                  backgroundColor:gradientFill,
+                  borderColor:"#000091",
+                  type:'line',
+                  pointRadius:8,
+                  pointBackgroundColor:"rgba(0, 0, 0, 0)",
+                  pointBorderColor:"rgba(0, 0, 0, 0)",
+                },
+                {
+                  data: self.dataset2,
+                  backgroundColor:gradientFill2,
+                  borderColor:"#df001b",
+                  type:'line',
+                  pointRadius:8,
+                  pointBackgroundColor:"rgba(0, 0, 0, 0)",
+                  pointBorderColor:"rgba(0, 0, 0, 0)",
+                }
+              ]
+          },
+          options: {
+            animation: {
+              easing: "easeInOutBack"
+            },
+            scales: {
+              xAxes: [{
+                gridLines: {
+                  color: "rgba(0, 0, 0, 0)",
+                },
+                ticks: {
+                  autoSkip: true,
+                  maxTicksLimit: xTickLimit,
+                  maxRotation: 0,
+                  minRotation: 0,
+                  callback: function(value) {
+                    return value.toString().substring(0,5);
+                  }
+                },
+              }],
+              yAxes: [{
+                gridLines: {
+                  color: "#e5e5e5",
+                  borderDash:[3]
+                },
+                ticks: {
+                  autoSkip: true,
+                  maxTicksLimit: 5
+                }   
+            }]
+          },
+          legend: {
+              display: false
+          },
+          tooltips:{
+            displayColors:false,
+            backgroundColor:"#6b6b6b",
+            callbacks: {
+              label: function(tooltipItems) { 
+                var int = self.convertStringToLocaleNumber(tooltipItems["value"])
+                return int+" "+self.units[tooltipItems["datasetIndex"]]
+              },
+              title: function(tooltipItems) { 
+                return tooltipItems[0]["label"]
+              },
+              labelTextColor: function(){
+                return "#eeeeee"
+              }
+            },
+          }
+        }
+      });
+      console.log(this.chart)
+    },
+
+    convertStringToLocaleNumber(string){
+      return parseInt(string).toLocaleString()
+    },
+
+    convertDateToHuman(string){
+      let date = new Date(string)
+      return date.toLocaleDateString()
+    }
+  
+  },
+
+  watch:{
+    selectedGeoCode:function(){
+      this.updateChart()
+    },
+    selectedGeoLevel:function(){
+      this.updateChart()
+    }
+  },
+
+  created(){
+    this.chartId = "myChart"+Math.floor(Math.random() * (1000));
+    this.widgetId = "widget"+Math.floor(Math.random() * (1000));
+    this.getData()
+  },
+
+  mounted(){
+    document.getElementById(this.widgetId).offsetWidth > 486 ? this.display='big' : this.display='small'
+    // 502px to break
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  
+  /* overload fonts path, to delete when parent has access */
+  @import "../../css/overload-fonts.css";
+  @import "../../css/dsfr.min.css";
+
+
+
+  .widget_container{
+
+    .r_col {
+      align-self:center;
+    }
+
+    .chart canvas {
+      max-width:100%;
+    }
+
+  }
+  
+</style>
