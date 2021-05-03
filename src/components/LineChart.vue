20a3c43c63b556fbe74ea65faeeefba170de2e96
nicolas <nicolas_boeuf@hotmail.fr>
2021-05-03 15:23:50 +0200

    plugging to soverein solution

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -56,7 +56,7 @@ export default {
   methods: {
 
     async getData () {
-      var url = "https://raw.githubusercontent.com/etalab/data-covid19-dashboard-widgets/master/data/"+this.indicateur+".json"
+      var url = "https://data.widgets.dashboard.covid19.data.gouv.fr/"+this.indicateur+".json"
       const dataRequest = await fetch(url)
       const data = await dataRequest.json()
       this.indicateur_data = data
