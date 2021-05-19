f93bc0df6f08fbfd559f6513b24c50fca0afe11a
LouisBellinger <loic.bellinger@gmail.com>
2021-05-19 16:52:38 +0200

    Updated maps & fixed separator

--- src/components/LeftCol.vue
+++ src/components/LeftCol.vue
@@ -13,11 +13,11 @@
         <div data-box="number">
           <p class="l_box_title fr-text--xs fr-mb-1w">Mise à jour : {{date}}</p>
 
-          <div class="indicateur_info" v-for="(n,i) in names" :key="n">
+          <div class="indicateur_info" :class="i>0 ? 'fr-mt-2w' : ''" v-for="(n,i) in names" :key="n">
             <p class="fr-text--sm fr-text--bold fr-mt-0 fr-mb-1w">{{names[i]}}</p>
             <div class="l_box_number_container">
               <p class="fr-text--lg fr-text--bold fr-mb-1v">{{convertNumberToHuman(values[i])}}</p>
-              <p class="l_box_trend flex fr-mb-3v fr-text--xs fr-text--bold" v-bind:class="{'down':isDown[i],'green':isGreen[i],'red':isRed[i],'blue':isBlue[i]}">
+              <p class="l_box_trend flex fr-mb-0 fr-text--xs fr-text--bold fr-px-1w fr-py-1v" v-bind:class="{'down':isDown[i],'green':isGreen[i],'red':isRed[i],'blue':isBlue[i]}">
                 <svg class="trend_ico" width="16" height="16" viewBox="0 0 24 24">
                   <path v-if="!isBlue[i]" d="M19.071 4.929c3.903 3.903 3.903 10.239 0 14.142-3.903 3.903-10.239 3.903-14.142 0-3.903-3.903-3.903-10.239 0-14.142 3.903-3.903 10.239-3.903 14.142 0zm-2.828 2.828H7.757l3.182 3.182-4.242 4.243 2.121 2.121 4.243-4.242 3.182 3.182V7.757z" transform="translate(-902 -5664) translate(902 5664)"/>
                   <path v-if="isBlue[i]" d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2zm4 11H8v2h8v-2zm0-4H8v2h8V9z" transform="translate(-1366 -5645) translate(1366 5645)"/>
@@ -29,11 +29,13 @@
         </div>
 
         <div v-if="map" class="sep fr-my-4w fr-my-md-3w"></div>
+        <div v-else class="sep-viz fr-my-4w fr-my-md-3w"></div>
         <div class="scale" v-if="map">
           <p class="l_box_title fr-text--xs fr-mb-1w">Légende</p>
-          <div class="scale_container">
-            <span class="min">{{convertNumberToHuman(min)}}</span>
-            <span class="max">{{convertNumberToHuman(max)}}</span>
+          <div class="scale_container"></div>
+          <div>
+            <span class="min fr-text--sm fr-text--bold fr-mb-0">{{convertNumberToHuman(min)}}</span>
+            <span class="max fr-text--sm fr-text--bold fr-mb-0">{{convertNumberToHuman(max)}}</span>
           </div>
         </div>
     </div>
@@ -121,15 +123,21 @@ export default {
 
   .l_col{
 
-    .sep {
+    .sep, .sep-viz {
       border-bottom:1px solid #E5E5E5;
+    }
+
+    @media (min-width: 62em) {
+      .sep-viz {
+        display: none;
+      }
     }
 
     .l_box_title{
       color: #6b6b6b;
     }
     .flex{
-      display: flex;
+      display: inline-flex;
       align-items: center;
     }
     .l_box_number_container {
@@ -143,6 +151,7 @@ export default {
         }
         &.green {
           color: #357941;
+          background-color: #d9ffeb;
           .trend_ico {
             path {
               fill: #357941;
@@ -151,6 +160,7 @@ export default {
         }
         &.red {
           color: #d80600;
+          background-color: #fff4f3;
           .trend_ico {
             path {
               fill: #d80600;
@@ -159,6 +169,7 @@ export default {
         }
         &.blue {
           color: #0768d5;
+          background-color: #f0f7ff;
           .trend_ico {
             path {
               fill: #0768d5;
@@ -169,22 +180,13 @@ export default {
     }
     .scale{
       .scale_container{
-        width: 100%;
-        height: 25px;
+        height: 1.5rem;
         background-color: red;
-        position: relative;
         background: linear-gradient(90deg, rgba(255, 199, 0,1) 0%, rgba(113, 88, 69, 1) 100%);
-        span{
-          position: absolute;
-          bottom: -25px;
-          font-weight: bold;
-          &.min{
-            left:0;  
-          }
-          &.max{
-            right: 0;
-          }
-        }
+      }
+      div:last-child {
+        display:flex;
+        justify-content: space-between;
       }
     }
 
