7202664f3f8c945f3152ecd7a0ed5626a3ec58b0
Erwan Daubert <erwan.daubert@gmail.com>
2021-06-09 17:50:20 +0200

    Refactoring pour afficher des graphiques

--- src/store/index.js
+++ src/store/index.js
@@ -1,53 +1,50 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import dataP from '../../donnee_jupiter/json/Entrées_de_jeunes_en_CIE.json'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    dep:[],
-    reg:[],
+    dep: [],
+    reg: [],
     promises: {},
     data: {},
-    user:{
-      selectedGeoLevel:"France",
-      selectedGeoCode:"01",
-      selectedGeoLabel:"France entière"
+    user: {
+      selectedGeoLevel: "France",
+      selectedGeoCode: "01",
+      selectedGeoLabel: "France entière"
     },
   },
   actions: {
-    getData ({commit, state}, indicator) {
+    getData({ commit, state }, indicator) {
       if (state.promises[indicator]) {
         return state.promises[indicator]
       }
-      /*
-      const url = "https://data.widgets.dashboard.covid19.data.gouv.fr/"+indicator+".json"
+      const url = "http://localhost:8080/json/" + indicator + ".json"
       const promise = fetch(url).then(res => {
         return res.json()
       }).then(data => {
-        commit('setData', {'indicator': indicator, 'data': data})
+        commit('setData', { 'indicator': indicator, 'data': data })
         return data
       })
-      commit('setPromise', {'indicator': indicator, 'promise': promise})
-      */
-      return dataP
+      commit('setPromise', { 'indicator': indicator, 'promise': promise })
+      return promise
     }
   },
   mutations: {
-    setPromise (state, payload) {
+    setPromise(state, payload) {
       state['promises'][payload['indicator']] = payload['promise']
     },
-    setData (state, payload) {
+    setData(state, payload) {
       state['data'][payload['indicator']] = payload['data']
     },
-    initDep (state,dep) {
+    initDep(state, dep) {
       state.dep = dep
     },
-    initReg (state,reg) {
+    initReg(state, reg) {
       state.reg = reg
     },
-    setUserChoices (state,payload){
+    setUserChoices(state, payload) {
       state.user.selectedGeoLevel = payload.level
       state.user.selectedGeoCode = payload.code
       state.user.selectedGeoLabel = payload.label
