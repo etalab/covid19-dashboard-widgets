7b25e5dcb8d38fb826ce67abfd3d6a22247d69b9
Nicolas Boeuf <nicolas_boeuf@hotmail.fr>
2021-09-30 10:47:51 +0200

    init starter kit

--- src/main.js
+++ src/main.js
@@ -3,16 +3,7 @@ import Vue from 'vue'
 import store from './store'
 import { getData } from './import.js'
 
-// import Map from './components/Map'
-import DataBox from './components/DataBox'
-import LineChart from './components/LineChart'
-import MultiLineChart from './components/MultiLineChart'
-import BarChart from './components/BarChart'
-import MapChart from './components/MapChart'
-import GeoList from './components/GeoList'
-import ThreeLineChart from './components/ThreeLineChart'
-import NewMultiLineChart from './components/NewMultiLineChart'
-import BarLineChart from './components/BarLineChart'
+import MyComponent from './components/MyComponent'
 
 import vueCustomElement from 'vue-custom-element'
 Vue.use(getData(store))
@@ -21,13 +12,4 @@ Vue.config.productionTip = false
 
 Vue.use(vueCustomElement)
 
-// Vue.customElement('da-map', Map)
-Vue.customElement('data-box', DataBox)
-Vue.customElement('line-chart', LineChart)
-Vue.customElement('multiline-chart', MultiLineChart)
-Vue.customElement('bar-chart', BarChart)
-Vue.customElement('map-chart', MapChart)
-Vue.customElement('geo-list', GeoList)
-Vue.customElement('threeline-chart', ThreeLineChart)
-Vue.customElement('new-multiline-chart', NewMultiLineChart)
-Vue.customElement('bar-line-chart', BarLineChart)
+Vue.customElement('my-component', MyComponent)
