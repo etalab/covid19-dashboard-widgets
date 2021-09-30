bfcf54ca742ed08166df2e063fc5c127781b0fed
Nicolas Boeuf <nicolas_boeuf@hotmail.fr>
2021-09-30 11:04:21 +0200

    revert changes

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>{{customtext}}</h1>
+  </div>
+</template>
+
+<script>
+import store from '@/store'
+import { mixin } from '@/utils.js'
+export default {
+  name: 'MyComponent',
+  mixins: [mixin],
+  data(){
+    return {
+      
+    }
+  },
+  props: {
+    customtext: String
+  },
+  computed: {
+    dataImport() {
+      return store.state.endImport
+    },
+  },
+  methods: {
+
+  },
+
+  watch:{
+    dataImport:function(){
+      console.log("dataImport over")
+      console.log(store.state.myData)
+    },
+  },
+
+  created(){
+    console.log("MyComponent created")
+  }
+
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -3,54 +3,17 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-const BASE_URL = 'https://data.widgets.dashboard.covid19.data.gouv.fr'
-// const BASE_URL = './dist'
-
 export default new Vuex.Store({
   state: {
-    dep: [],
-    reg: [],
-    promises: {},
-    data: {},
-    user: {
-      selectedGeoLevel: 'France',
-      selectedGeoCode: '01',
-      selectedGeoLabel: 'France entière'
-    }
-  },
-  actions: {
-    getData ({ commit, state }, indicator) {
-      if (state.promises[indicator]) {
-        return state.promises[indicator]
-      }
-      const url = `${BASE_URL}/${indicator}.json`
-      const promise = fetch(url).then(res => {
-        return res.json()
-      }).then(data => {
-        commit('setData', { indicator: indicator, data: data })
-        return data
-      })
-      commit('setPromise', { indicator: indicator, promise: promise })
-      return promise
-    }
+    myData:undefined,
+    endImport:false
   },
   mutations: {
-    setPromise (state, payload) {
-      state.promises[payload.indicator] = payload.promise
+    initializeData (state,data) {
+      state.myData = data
     },
-    setData (state, payload) {
-      state.data[payload.indicator] = payload.data
-    },
-    initDep (state, dep) {
-      state.dep = dep
-    },
-    initReg (state, reg) {
-      state.reg = reg
-    },
-    setUserChoices (state, payload) {
-      state.user.selectedGeoLevel = payload.level
-      state.user.selectedGeoCode = payload.code
-      state.user.selectedGeoLabel = payload.label
+    endImport(state,value){
+      state.endImport = value
     }
   }
 })
